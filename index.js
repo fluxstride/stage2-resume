005acb83999672f884f39927f0261162dde6c8ac
adekoya samuel <adekoyasamuel52@gmail.com>
2021-08-19 20:08:36 +0100

    -

---
+++ index.js
@@ -0,0 +1,74 @@
+const { response } = require("express");
+const express = require("express");
+const mongoose = require("mongoose");
+const mailer = require("nodemailer");
+const router = require("express").Router();
+const port = process.env.PORT || 3000;
+
+mongoose.connect("mongodb://localhost:27017/test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const responseModel = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+});
+
+const Response = mongoose.model("response", responseModel);
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(express.urlencoded());
+router.get("/", (req, res) => {
+    res.render("index");
+});
+
+router.post("/submit", (req, res) => {
+    const response = req.body;
+    const userResponse = new Response({...response });
+
+    var transporter = mailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "samueladexx@gmail.com",
+            pass: "09030316508",
+        },
+    });
+
+    var mailOptions = {
+        attachments: [{ filename: "1.txt", content: "hello world" }],
+        from: "Samuel Adekoya <samueladexx@gmail.com>",
+        to: response.email,
+        subject: "CONTACT RESPONSE",
+        html: `<p style=" font-size: 1.2rem;font-family: Poppins; line-height: 2">Thank you ${response.name} for reaching out to me. I will get back to you as soon as possible</p>`,
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Email sent: " + info.response);
+        }
+    });
+
+    console.log(response);
+    // userResponse.save(() => { console.log("response saved") })
+    res.render("submit", { name: response.name });
+});
+router.post("/", (req, res) => {});
+
+app.use(router);
+app.listen(port, console.log(`server running on port ${port}`));
